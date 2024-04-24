7984b6be004c527254b40e5e6122644abe5a7ba2
Joseph McHugh <joseph.mchugh@volvo.com>
2024-04-23 21:51:19 -0400

    Commit 4/23/24 9:50pm

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,7 @@ public class Application {
 
         Sequence sequence = new Sequence();
 
+        //The sequence Object is being updated by two different threads:
         Worker worker1 = new Worker(sequence);
         worker1.start();
         Worker worker2 = new Worker(sequence);
--- src/Sequence.java
+++ src/Sequence.java
@@ -2,9 +2,19 @@ public class Sequence {
 
     private int value = 0;
 
-    public int getNext(){
-        value++;
-        return value;
+    public int getNext() {
+        //add synchronized like below to put a 'lock' on the code so it is completed
+        synchronized (this) {
+            value++;
+            return value;
+
+        }
 
     }
 }
+            /**  Another way to implement synchronized on the whole method:
+             public synchronized int getNex(){
+                    value++;
+                    return value;
+             }
+            **/
